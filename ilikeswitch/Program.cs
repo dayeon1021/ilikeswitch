7a8ec81fa893c772d02c9bb5512b58037b362bf6
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-06 22:34:02 +0900

    2020.05.06 실습보고서 7번

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -10,6 +10,24 @@ namespace ilikeswitch
     {
         static void Main(string[] args)
         {
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 12점");
+                    break;
+                case 2:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 10점");
+                    break;
+                case 4:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18점");
+                    break;
+            }
         }
     }
 }
